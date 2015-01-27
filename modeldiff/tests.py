2e5bad7809bd2e49575fb6809a0c997ad4fd14a8
abusquets <abusquets@gmail.com>
2015-01-27 22:11:48 +0000

    Changes for postgres compatibility

--- modeldiff/tests.py
+++ modeldiff/tests.py
@@ -26,12 +26,12 @@ class GMT0(tzinfo):
 class ModeldiffTests(TestCase):
 
     def setUp(self):
-        PersonModel.objects.create(name='Foo', surname='Doe',
+        self.person = PersonModel.objects.create(name='Foo', surname='Doe',
                                    birthdate=date(2007, 12, 5),
                                    updated_at=datetime(2015, 1, 7, 22, 0, 10, 
                                     292032, GMT0()))
         
-        PersonGeoModel.objects.create(name='Foo', surname='Doe',
+        self.persongeo = PersonGeoModel.objects.create(name='Foo', surname='Doe',
                                    birthdate=date(2007, 12, 5),
                                    updated_at=datetime(2015, 1, 7, 22, 0, 10, 
                                     292032, GMT0()),
@@ -52,10 +52,10 @@ class ModeldiffTests(TestCase):
         print >> sys.stderr, '  ---------'
 
     def test_creation_diff_exists(self):
-        diff = Modeldiff.objects.all()[0]
+        diff = Modeldiff.objects.all().order_by('-id')[0]
         self.assertEqual(diff.id, 1)
         self.assertEqual(diff.action, 'add')
-        self.assertEqual(diff.model_id, 1)
+        self.assertEqual(diff.model_id, self.person.id)
         self.assertEqual(diff.model_name, 'modeldiff.PersonModel')
         self.assertEqual(diff.old_data, '')
          
@@ -66,21 +66,16 @@ class ModeldiffTests(TestCase):
                           })
  
     def test_modify_model(self):
-        person = PersonModel.objects.get(pk=1)
-        self.assertEqual(person.pk, 1)
-        person.name = 'Bar'
-        person.birthdate = date(2010, 10, 1)
-        person.updated_at = datetime(2015, 1, 7, 23, 12, 11, 292032, GMT0())
-        person.save()
-         
-        person.name = 'John'
-        person.birthdate = date(2008, 11, 2)
-        person.updated_at = datetime(2015, 1, 7, 23, 39, 13, 292032, GMT0())
-        person.save()
+
+        self.person.name = 'Bar'
+        self.person.birthdate = date(2010, 10, 1)
+        self.person.updated_at = datetime(2015, 1, 7, 23, 12, 11, 292032, GMT0())
+        self.person.save()
   
-        diff = Modeldiff.objects.get(pk=2)
+        diff = Modeldiff.objects.all().order_by('-id')[0]
+
         self.assertEqual(diff.action, 'update')
-        self.assertEqual(diff.model_id, 1)
+        self.assertEqual(diff.model_id, self.person.id)
         self.assertEqual(json.loads(diff.old_data),
                          {u'name': u'Foo', u'surname': u'Doe',
                           u'birthdate': '2007-12-05',
@@ -89,10 +84,16 @@ class ModeldiffTests(TestCase):
                             'birthdate': '2010-10-01',
                             'updated_at': '2015-01-07 23:12:11.292032+0000',                               
                             })
-  
-        diff = Modeldiff.objects.get(pk=3)
+        
+        self.person.name = 'John'
+        self.person.birthdate = date(2008, 11, 2)
+        self.person.updated_at = datetime(2015, 1, 7, 23, 39, 13, 292032, GMT0())
+        self.person.save()
+          
+        diff = Modeldiff.objects.all().order_by('-id')[0]
+        
         self.assertEqual(diff.action, 'update')
-        self.assertEqual(diff.model_id, 1)
+        self.assertEqual(diff.model_id, self.person.id)
         self.assertEqual(json.loads(diff.old_data),
                          {'name': 'Bar', 'surname': 'Doe',
                           u'birthdate': '2010-10-01',
@@ -103,28 +104,27 @@ class ModeldiffTests(TestCase):
                             })
   
     def test_delete_model(self):
-        person = PersonModel.objects.get(pk=1)
-        self.assertEqual(person.pk, 1)
-        person.delete()
-   
-        diff = Modeldiff.objects.get(pk=2)
+
+        person_id = self.person.id
+        self.person.delete()
+    
+        diff = Modeldiff.objects.all().order_by('-id')[0]
         self.assertEqual(diff.action, 'delete')
-        self.assertEqual(diff.model_id, 1)
+        self.assertEqual(diff.model_id, person_id)
         self.assertEqual(json.loads(diff.old_data),
                          {u'name': u'Foo', u'surname': u'Doe',
                           u'birthdate': '2007-12-05',
                           u'updated_at': '2015-01-07 22:00:10.292032+0000'})
         self.assertEqual(diff.new_data, '')
- 
+  
     def test_set_to_none(self):
-        person = PersonModel.objects.get(pk=1)
-        self.assertEqual(person.pk, 1)
-        person.surname = None
-        person.save()
-   
-        diff = Modeldiff.objects.get(pk=2)
+
+        self.person.surname = None
+        self.person.save()
+    
+        diff = Modeldiff.objects.all().order_by('-id')[0]
         self.assertEqual(diff.action, 'update')
-        self.assertEqual(diff.model_id, 1)
+        self.assertEqual(diff.model_id, self.person.id)
         self.assertEqual(json.loads(diff.old_data),
                          {u'name': u'Foo', u'surname': u'Doe',
                           u'birthdate': u'2007-12-05',
@@ -133,53 +133,48 @@ class ModeldiffTests(TestCase):
                          {u'surname': None})
      
     def test_update_parent_field_model(self):
-            
-        person = PersonModel.objects.get(pk=1)
-        self.assertEqual(person.pk, 1)
-    
-        person_property = PersonPropertyModel.objects.create(person=person, 
+     
+        person_property = PersonPropertyModel.objects.create(person=self.person, 
             address="Carme 15, Girona")
-      
+       
         diffs = Modeldiff.objects.all().order_by('-id')[:2]
-           
+            
         self.assertEqual(diffs[0].action, 'update')
         self.assertEqual(diffs[0].model_name, 'modeldiff.PersonModel')
-        self.assertEqual(diffs[0].model_id, person_property.person.pk)
-        
+        self.assertEqual(diffs[0].model_id, person_property.person.id)
+         
         self.assertEqual(diffs[1].action, 'add')
         self.assertEqual(diffs[1].model_name, 'modeldiff.PersonPropertyModel')
-        self.assertEqual(diffs[1].model_id, person_property.pk)
-
+        self.assertEqual(diffs[1].model_id, person_property.id)
+ 
     def test_delete_parent_field_model(self):
-            
-        person = PersonModel.objects.get(pk=1)
-        self.assertEqual(person.pk, 1)
-    
-        person_property = PersonPropertyModel.objects.create(person=person, 
+     
+        person_property = PersonPropertyModel.objects.create(person=self.person, 
             address="Carme 15, Girona")
-        person_property_pk = person_property.person.pk
+        person_property_id = person_property.id
         person_property.delete()
-      
+       
         diffs = Modeldiff.objects.all().order_by('-id')[:2]
-           
+            
         self.assertEqual(diffs[0].action, 'update')
         self.assertEqual(diffs[0].model_name, 'modeldiff.PersonModel')
-        self.assertEqual(diffs[0].model_id, person.pk)
-        
+        self.assertEqual(diffs[0].model_id, self.person.id)
+         
         self.assertEqual(diffs[1].action, 'delete')
         self.assertEqual(diffs[1].model_name, u'modeldiff.PersonPropertyModel')
-        self.assertEqual(diffs[1].model_id, person_property_pk)
-
-            
+        self.assertEqual(diffs[1].model_id, person_property_id)
+ 
+             
     #Geo tests
     def test_geo_creation_diff_exists(self):
-        diff = Geomodeldiff.objects.all()[0]
-        self.assertEqual(diff.id, 1)
+
+        diff = Geomodeldiff.objects.all().order_by('-id')[0]
+
         self.assertEqual(diff.action, 'add')
-        self.assertEqual(diff.model_id, 1)
+        self.assertEqual(diff.model_id, self.persongeo.id)
         self.assertEqual(diff.model_name, 'modeldiff.PersonGeoModel')
         self.assertEqual(diff.old_data, '')
-          
+           
         self.assertEqual(json.loads(diff.new_data),
                          {'name': 'Foo', 'surname': 'Doe', 
                           'birthdate': '2007-12-05',
@@ -188,23 +183,17 @@ class ModeldiffTests(TestCase):
                           })
         self.assertEqual(precision_wkt(diff.the_geom, 8),
                          'POINT(0.00000000 0.00000000)')
-  
-    def test_geo_modify_model(self):
-        person = PersonGeoModel.objects.get(pk=1)
-        self.assertEqual(person.pk, 1)
-        person.name = 'Bar'
-        person.birthdate = date(2010, 10, 1)
-        person.updated_at = datetime(2015, 1, 7, 23, 12, 11, 292032, GMT0())
-        person.save()
-          
-        person.name = 'John'
-        person.birthdate = date(2008, 11, 2)
-        person.updated_at = datetime(2015, 1, 7, 23, 39, 13, 292032, GMT0())
-        person.save()
    
-        diff = Geomodeldiff.objects.get(pk=2)
+    def test_geo_modify_model(self):
+
+        self.persongeo.name = 'Bar'
+        self.persongeo.birthdate = date(2010, 10, 1)
+        self.persongeo.updated_at = datetime(2015, 1, 7, 23, 12, 11, 292032, GMT0())
+        self.persongeo.save()
+
+        diff = Geomodeldiff.objects.all().order_by('-id')[0]
         self.assertEqual(diff.action, 'update')
-        self.assertEqual(diff.model_id, 1)
+        self.assertEqual(diff.model_id, self.persongeo.id)
         self.assertEqual(json.loads(diff.old_data),
                          {u'name': u'Foo', u'surname': u'Doe',
                           u'birthdate': '2007-12-05',
@@ -214,10 +203,15 @@ class ModeldiffTests(TestCase):
                             'birthdate': '2010-10-01',
                             'updated_at': '2015-01-07 23:12:11.292032+0000',                               
                             })
-   
-        diff = Geomodeldiff.objects.get(pk=3)
+                   
+        self.persongeo.name = 'John'
+        self.persongeo.birthdate = date(2008, 11, 2)
+        self.persongeo.updated_at = datetime(2015, 1, 7, 23, 39, 13, 292032, GMT0())
+        self.persongeo.save()
+    
+        diff = Geomodeldiff.objects.all().order_by('-id')[0]
         self.assertEqual(diff.action, 'update')
-        self.assertEqual(diff.model_id, 1)
+        self.assertEqual(diff.model_id, self.persongeo.id)
         self.assertEqual(json.loads(diff.old_data),
                          {'name': 'Bar', 'surname': 'Doe',
                           u'birthdate': '2010-10-01',
@@ -227,32 +221,31 @@ class ModeldiffTests(TestCase):
                             'birthdate': '2008-11-02',
                             'updated_at': '2015-01-07 23:39:13.292032+0000',                               
                             })
-   
-    def test_geo_delete_model(self):
-        person = PersonGeoModel.objects.get(pk=1)
-        self.assertEqual(person.pk, 1)
-        person.delete()
     
-        diff = Geomodeldiff.objects.get(pk=2)
+    def test_geo_delete_model(self):
+
+        persongeo_id = self.persongeo.id
+        self.persongeo.delete()
+     
+        diff = Geomodeldiff.objects.all().order_by('-id')[0]
         self.assertEqual(diff.action, 'delete')
-        self.assertEqual(diff.model_id, 1)
+        self.assertEqual(diff.model_id, persongeo_id)
         self.assertEqual(json.loads(diff.old_data),
                          {u'name': u'Foo', u'surname': u'Doe',
                           u'birthdate': '2007-12-05',
                           u'updated_at': '2015-01-07 22:00:10.292032+0000',
                           u'the_geom': u'POINT(0.00000000 0.00000000)'})
         self.assertEqual(diff.new_data, '')
-  
+   
     def test_geo_set_to_none(self):
-        person = PersonGeoModel.objects.get(pk=1)
-        self.assertEqual(person.pk, 1)
-        person.surname = None
-        person.the_geom = None
-        person.save()
-    
-        diff = Geomodeldiff.objects.get(pk=2)
+
+        self.persongeo.surname = None
+        self.persongeo.the_geom = None
+        self.persongeo.save()
+     
+        diff = Geomodeldiff.objects.all().order_by('-id')[0]
         self.assertEqual(diff.action, 'update')
-        self.assertEqual(diff.model_id, 1)
+        self.assertEqual(diff.model_id, self.persongeo.id)
         self.assertEqual(json.loads(diff.old_data),
                          {u'name': u'Foo', u'surname': u'Doe',
                           u'birthdate': u'2007-12-05',
@@ -260,49 +253,44 @@ class ModeldiffTests(TestCase):
                           u'the_geom': u'POINT(0.00000000 0.00000000)'})
         self.assertEqual(json.loads(diff.new_data),
                          {u'surname': None, u'the_geom': None})
-     
+      
     def test_update_parent_field_geo_model(self):
-           
-        person = PersonGeoModel.objects.get(pk=1)
-        self.assertEqual(person.pk, 1)
-   
+
         person_property = PersonPropertyForGeoModel.objects.create(
-            person=person, address="Carme 15, Girona")
-     
-        diff = Geomodeldiff.objects.latest('id')
-          
+            person=self.persongeo, address="Carme 15, Girona")
+       
+        diff = Geomodeldiff.objects.all().order_by('-id')[0]
+            
         self.assertEqual(diff.action, 'update')
         self.assertEqual(diff.model_name, 'modeldiff.PersonGeoModel')
-        self.assertEqual(diff.model_id, person_property.person.pk)
-       
-        self.assertEqual(diff.new_data, '{}')
+        self.assertEqual(diff.model_id, person_property.person.id)
           
-       
-        diff = Modeldiff.objects.latest('id')
+        self.assertEqual(diff.new_data, '{}')
+        
+        diff = Modeldiff.objects.all().order_by('-id')[0]
         self.assertEqual(diff.action, 'add')
         self.assertEqual(diff.model_name, 'modeldiff.PersonPropertyForGeoModel')
-        self.assertEqual(diff.model_id, person_property.pk)
-
+        self.assertEqual(diff.model_id, person_property.id)
+ 
+ 
     def test_geo_delete_parent_field_model(self):
-           
-        person = PersonGeoModel.objects.get(pk=1)
-        self.assertEqual(person.pk, 1)
-   
+    
         person_property = PersonPropertyForGeoModel.objects.create(
-            person=person, address="Carme 15, Girona")
-        
-        person_property_pk = person_property.person.pk
+            person=self.persongeo, address="Carme 15, Girona")
+         
+        person_property_id = person_property.id
         person_property.delete()
-             
-        diff = Geomodeldiff.objects.latest('id')
-          
+              
+        diff = Geomodeldiff.objects.all().order_by('-id')[0]
+           
         self.assertEqual(diff.action, 'update')
         self.assertEqual(diff.model_name, 'modeldiff.PersonGeoModel')
-        self.assertEqual(diff.model_id, person_property.person.pk)
-       
+        self.assertEqual(diff.model_id, self.persongeo.id)
+        
         self.assertEqual(diff.new_data, '{}')
-       
-        diff = Modeldiff.objects.latest('id')
+        
+        diff = Modeldiff.objects.all().order_by('-id')[0]
         self.assertEqual(diff.action, 'delete')
         self.assertEqual(diff.model_name, 'modeldiff.PersonPropertyForGeoModel')
-        self.assertEqual(diff.model_id, person_property_pk) 
+        self.assertEqual(diff.model_id, person_property_id)
+    
