ed4047a404c5d6e4d912bd12bf5ab28c81811c06
Manel Clos <manelclos@gmail.com>
2017-04-13 13:19:17 +0200

    fix some problems with tests

--- tests/settings.py
+++ tests/settings.py
@@ -25,7 +25,6 @@ DATABASES = {
 }
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
